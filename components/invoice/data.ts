41000942863adcfe86bf61a497c4c1f5bc8ecd6b
hiyasin <c223059@ugrad.iiuc.ac.bd>
2025-11-18 23:41:15 +0600

    Practice proof of concept generate invoice pdf

---
+++ components/invoice/data.ts
@@ -0,0 +1,57 @@
+export const companyInfo = {
+  name: "Bobbin",
+  phone: "01609 469623",
+  mail: "info@bobbin.com.bd",
+  addressLines: [
+    "",
+  ],
+};
+
+export const invoiceMeta = {
+  number: "INV-2025-019",
+  date: "19 October 2025",
+};
+
+export const recipient = {
+  name: "Kazi Md Mostafa Raihan",
+  phone: "01313703014",
+  company: "British American Tobacco Bangladesh (BAT)",
+  address:
+    "Flat no: C-1, Dom Inno Abrigo, 20 Gaus Nagar road, New Eskaton ,Dhaka-1000",
+};
+
+export const subject = "T-shirt and Documents";
+
+export const tableData = [
+  {
+    sl: 1,
+    description: "Cotton t-shirt",
+    itemType: "Garments",
+    quantity: 2,
+    unitPrice: 300.0,
+    total: 600.0,
+  },
+  {
+    sl: 2,
+    description: "Agreement Paper",
+    itemType: "Documents",
+    quantity: 4,
+    unitPrice: 50.0,
+    total: 200.0,
+  },
+];
+
+export const totalData = [
+  {
+    label: "Subtotal",
+    value: 800.00,
+  },
+  {
+    label: "Tax",
+    value: 0.00,
+  },
+  {
+    label: "Total (BDT)",
+    value: 800.00,
+  },
+];
